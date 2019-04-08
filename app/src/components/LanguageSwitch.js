c51a813a89287d0480d90862f8528f3ad457d2b5
Joosep Alviste <joosep.alviste@gmail.com>
2019-04-08 11:19:44 +0300

    Move LanguageSwitch component to use i18next

--- app/src/components/LanguageSwitch.js
+++ app/src/components/LanguageSwitch.js
@@ -1,40 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { Button } from 'reactstrap';
+import { useTranslation } from 'react-i18next';
 
-import { setActiveLanguage, applicationSelectors } from 'ducks/application';
 import SETTINGS from 'settings';
 
 
-const LanguageSwitch = ({ activeLanguage, onSwitch }) => (
-    <>
-        <p>
-            Active language: {activeLanguage}
-        </p>
-        {SETTINGS.LANGUAGE_ORDER.map((languageCode) => (
-            <Button key={languageCode} onClick={() => onSwitch(languageCode)} className="mr-2">
-                {SETTINGS.LANGUAGES[languageCode]}
-            </Button>
-        ))}
-    </>
-);
-
-LanguageSwitch.propTypes = {
-    activeLanguage: PropTypes.string.isRequired,
-    onSwitch: PropTypes.func.isRequired,
+const LanguageSwitch = () => {
+    const { i18n } = useTranslation();
+    return (
+        <>
+            <p>
+                Active language: {i18n.language}
+            </p>
+            {SETTINGS.LANGUAGE_ORDER.map((languageCode) => (
+                <Button
+                    key={languageCode}
+                    onClick={() => i18n.changeLanguage(languageCode)}
+                    className="mr-2"
+                >
+                    {SETTINGS.LANGUAGES[languageCode]}
+                </Button>
+            ))}
+        </>
+    );
 };
 
-
-const mapStateToProps = (state) => ({
-    activeLanguage: applicationSelectors.activeLanguage(state),
-});
-
-const mapDispatchToProps = (dispatch) => ({
-    onSwitch: (languageCode) => dispatch(setActiveLanguage(languageCode)),
-});
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(LanguageSwitch);
+export default LanguageSwitch;
